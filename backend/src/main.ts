2ebc6c642585604819c90e2e2461bcb161435d94
gricardov <gricardov@gmail.com>
2025-08-14 08:31:16 -0500

    dynamic port backedn

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000);
-  console.log('🚀 Servidor ejecutándose en http://localhost:3000');
+  await app.listen(process.env.PORT || 3000);
+  console.log(`🚀 Servidor ejecutándose en http://localhost:${process.env.PORT || 3000}`);
 }
 bootstrap();
